a2d590f7d5b4ad4d0aa32d92999ae61d66b4a304
Firaol Mk <firaolmekbib511@gmail.com>
2024-04-19 15:24:43 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creating file in tmp/ with specified permission
 file { '/tmp/school':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I Love Puppet',
+    ensure  => file,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I Love Puppet',
 }
