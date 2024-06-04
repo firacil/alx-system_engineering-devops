903742cd0d0035bb6e32d8eda20dbd33cfea1fc4
Firaol Mk <firaolmekbib511@gmail.com>
2024-06-04 15:02:29 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,13 +12,15 @@ def number_of_subscribers(subreddit):
     """
 
     if subreddit is None or not isinstance(subreddit, str):
-        user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-        response = get(url, headers=user_agent)
-        results = response.json()
+        return 0
+        
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
-        try:
-            return results.get('data').get('subscribers')
+    try:
+        return results.get('data').get('subscribers')
 
-        except Exception:
-            return 0
+    except Exception:
+        return 0
