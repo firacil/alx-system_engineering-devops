49c63d00fb325638f79cab0268128b1ba926ee16
Firaol Mk <firaolmekbib511@gmail.com>
2024-06-04 14:57:10 -0700

    updated

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+query number of subscriberss for given subreddit
+"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the reddit Api and return number
+    of subscribers for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        response = get(url, headers=user_agent)
+        results = response.json()
+
+        try:
+            return results.get('data').get('subscribers')
+
+        except Exception:
+            return 0
